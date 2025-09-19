1115269e2b65531d498b83a2e1d70a4fbacb6fd6
Russell161 <rleggjunior@gmail.com>
2025-09-19 17:53:05 -0600

    added js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
-import { getParkData } from "./parkService.mjs";
+import { getParkData } from './parkService.mjs';
 
 const parkData = getParkData();
+
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+document.title = parkData.fullName;
+
+const heroImage = document.querySelector(".hero-banner > img");
+heroImage.src = parkData.images[0].url;
+heroImage.alt = parkData.images[0].altText;
+
+const content = document.querySelector(".hero-banner .content");
+content.innerHTML = `
+  <a href="${parkData.url}" class="park-title">${parkData.name}</a>
+  <p class="subheader">
+    <span>${parkData.designation}</span>
+    <span>${parkData.states}</span>
+  </p>
+`;
